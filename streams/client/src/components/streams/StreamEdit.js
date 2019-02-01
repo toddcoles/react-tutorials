55df80817a80c37f4b7c644282cf1f6cded0a9df
toddcoles <todd_coles@hotmail.com>
2019-02-01 13:35:43 -0700

    Full CRUD

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './streamForm';
+import _ from 'lodash';
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    // console.log(this.props);
+    if (!this.props.stream) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+        />
+        {/* {this.props.stream.title} */}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  // console.log(ownProps);
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
